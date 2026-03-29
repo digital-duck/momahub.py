ecec92f1c258a4150d8805a452f70df2cd3cf6d5
Gong,Wen <wen.g.gong@gmail.com>
2026-03-29 08:02:24 -0400

    refine

---
+++ scripts/ollama-pull-momagrid.sh
@@ -0,0 +1,29 @@
+
+# baseline
+#===================
+ollama pull tinyllama                  # 637 MB    17 months ago    
+ollama pull llama3.2                   # 2.0 GB    17 months ago    
+
+# fundational
+#===================
+ollama pull deepseek-r1                # 4.7 GB    14 months ago    
+ollama pull gemma3                     # 3.3 GB    10 months ago    
+ollama pull mistral                    # 4.1 GB    14 months ago    
+ollama pull phi3                       # 2.2 GB    3 weeks ago      
+ollama pull phi4-mini                  # 2.5 GB    3 weeks ago      
+ollama pull qwen3                      # 5.2 GB    10 months ago    
+
+ollama pull qwen2.5                    # 4.7 GB    14 months ago    
+ollama pull duckdb-nsql                # 3.8 GB    14 months ago    
+ollama pull mathstral                  # 4.1 GB    14 months ago    
+ollama pull qwen2.5-coder              # 4.7 GB    14 months ago    
+ollama pull qwen2-math                 # 4.4 GB    14 months ago    
+ollama pull starcoder2:7b              # 4.0 GB    14 months ago    
+
+# embedding
+#===================
+ollama pull embeddinggemma             # 621 MB    6 months ago     
+ollama pull snowflake-arctic-embed2    # 1.2 GB    14 months ago    
+ollama pull nomic-embed-text           # 274 MB    14 months ago    
+ollama pull qwen3-embedding:0.6b       # 639 MB    6 months ago     
+ollama pull qwen3-embedding:4b         # 2.5 GB    6 months ago     
